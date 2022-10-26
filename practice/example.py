54259a04cdb4bf81346f1e1b9b64a2d6428bc165
Valentyn <freundzzz777@gmail.com>
2022-10-26 16:01:07 +0300

    trash

---
+++ practice/example.py
@@ -0,0 +1,12 @@
+# n = int(input())
+# a = n % 10
+# print(a)
+
+# a = int(input("Input the number: "))
+# n = 0
+# while a > 0:
+#     z = a % 10
+#     a //= 10
+#     n *= 10
+#     n += z
+# print(n)
