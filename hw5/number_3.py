35bb2b2fd3b60cdb75732dac05a7141b2bb32efe
Valentyn <freundzzz777@gmail.com>
2022-10-29 11:37:10 +0300

    Homework № 5 edit

--- hw5/number_3.py
+++ hw5/number_3.py
@@ -1,6 +1,6 @@
 lst = []
 sum_, ser = 0, 0
-min, max = 1000000, 0
+min_, max_ = 1000000, 0
 even, not_even = 0, 0
 while True:
     numb = int(input('Введіть число: '))
@@ -13,20 +13,21 @@ while True:
         # print(lst)
 for i in range(0, len(lst)):
     sum_ += lst[i]
-    if lst[i] >= max:
-        max = lst[i]
+    if lst[i] >= max_:
+        max_ = lst[i]
 
 for i in range(0, len(lst)):
-    if lst[i] <= min:
-        min = lst[i]
+    if lst[i] <= min_:
+        min_ = lst[i]
 
 for i in range(0, len(lst)):
     if lst[i] % 2 == 0:
         even += 1
     else:
         not_even += 1
-
+# print('Максимальне значення =', max(lst))
+# print('Мінімальне значення =', min(lst))
 ser = sum_/len(lst)
 print(f'Сума = {sum_} \nСереднє арифметичне = %.2f' % ser)
-print(f'Максимальне значення = {max} \nМінімальне значення = {min}')
+print(f'Максимальне значення = {max_} \nМінімальне значення = {min_}')
 print(f'Парних чисел = {even} \nНе парних чисел = {not_even}')
