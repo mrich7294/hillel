789be3a4ae56e9f8222c43ff0868c584f11217c2
Valentyn <freundzzz777@gmail.com>
2022-11-03 17:54:23 +0200

    Homework № 6 edit

--- hw6/two_words_1.py
+++ hw6/two_words_1.py
@@ -1,8 +1,10 @@
-word_1, word_2 = input('Введіть два слова через пробіл: ').split()
+lst = []
+lst = input('Введіть два слова через пробіл: ').split()
 while True:
-    if word_1.isalpha() is not True or word_2.isalpha() is not True:
+    if len(lst) < 2 or len(lst) >= 3 or lst[0].isalpha() is not True or lst[1].isalpha() is not True:
         print('Зверніть увагу!!! Необхідно ввести два слова без розділових знаків та цифр через пробіл.')
-        word_1, word_2 = input('Введіть два слова через пробіл: ').split()
-    elif word_1.isalpha() is True and word_2.isalpha() is True:
+        lst = input('Введіть два слова через пробіл: ').split()
+    elif lst[0].isalpha() is True and lst[1].isalpha() is True:
+        word_1, word_2 = lst[0], lst[1]
         print(word_1[::-1].capitalize(), word_2[::-1].capitalize())
         break
