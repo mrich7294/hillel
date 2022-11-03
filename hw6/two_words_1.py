5b5b20c3a1dc419ea966dc95c3e292dc547721f8
Valentyn <freundzzz777@gmail.com>
2022-11-03 18:21:47 +0200

    Homework № 6 edit1

--- hw6/two_words_1.py
+++ hw6/two_words_1.py
@@ -1,4 +1,5 @@
 lst = []
+
 lst = input('Введіть два слова через пробіл: ').split()
 while True:
     if len(lst) < 2 or len(lst) >= 3 or lst[0].isalpha() is not True or lst[1].isalpha() is not True:
@@ -7,4 +8,5 @@ while True:
     elif lst[0].isalpha() is True and lst[1].isalpha() is True:
         word_1, word_2 = lst[0], lst[1]
         print(word_1[::-1].capitalize(), word_2[::-1].capitalize())
+
         break
