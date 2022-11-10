cf50afe1a34a739cf32f6e90aa410ee6db90b88f
Valentyn <freundzzz777@gmail.com>
2022-11-10 20:44:37 +0200

    Homework № 8 edit

--- hw8/list_number_1.py
+++ hw8/list_number_1.py
@@ -1,9 +1,10 @@
 import random
-list_ = []
+# list_ = []
 new_list = []
-list_ = [(random.randint(10, 250)) for i in range(20)]
-print('Вхідні дані: \n', list_)
-for j in range(len(list_)):
-    if list_[j] % 20 != 0:
-        new_list.append(list_[j])
-print('Результат: \n', new_list)
+list_1 = [(random.randint(10, 250)) for i in range(20)]
+print('Вхідні дані: \n', list_1)
+# for j in range(len(list_1)):
+#     if list_1[j] % 20 != 0:
+#         new_list.append(list_1[j])
+list_1 = [list_1[j] for j in range(len(list_1)) if list_1[j] % 20 != 0]
+print('Результат: \n', list_1)
