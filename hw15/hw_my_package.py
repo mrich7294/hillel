18bad7f408002be908829a428533a1724c0b4d00
Valentyn <freundzzz777@gmail.com>
2022-12-06 13:00:16 +0200

    Homework № 15 edit

--- hw15/hw_my_package.py
+++ hw15/hw_my_package.py
@@ -3,18 +3,21 @@ import time
 from my_package.sort import *
 
 if __name__ == "__main__":
-    lst = [random.randint(0, 50) for _ in range(0, 5000)]
+    n = 5000
+    lst = [random.randint(0, 50) for _ in range(0, n)]
     # print(lst)
     t1 = time.time()
     my_sort.selection_sort(lst)
     t2 = time.time()
-    print('selection_sort = ', t2 - t1)
+    print('Sorting execution time for selection_sort = ', t2 - t1)
+    # print(f'Sorting execution time for selection_sort = {t2 - t1}\n{lst}')
     t1 = time.time()
     my_sort.buble_sort(lst)
     t2 = time.time()
-    print('buble_sort = ', t2 - t1)
+    print('Sorting execution time for buble_sort = ', t2 - t1)
+    # print(f'Sorting execution time for buble_sort = {t2 - t1}\n{lst}')
     t1 = time.time()
     my_sort.rock_sort(lst)
     t2 = time.time()
-    print('rock_sort = ', t2 - t1)
-
+    print('Sorting execution time for rock_sort = ', t2 - t1)
+    # print(f'Sorting execution time for rock_sort = {t2 - t1}\n{lst}')
